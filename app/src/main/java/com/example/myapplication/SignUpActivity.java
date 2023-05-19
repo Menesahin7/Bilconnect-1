4cb15ff14064ee857188a4792ba529e25852bdd0
M.Enes <menesahin7@gmail.com>
2023-05-19 22:44:53 +0300

    First Screens of Application
    Log in, sign up and forgot password screens' UI designs have completed

--- app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -2,13 +2,55 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText editTextTextPersonName;
+    EditText editTextTextEmailAddress;
+    EditText editTextTextPassword;
+    EditText editTextTextConfPassword;
+    Button btnSignUp;
+    TextView btnTextViewLogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        editTextTextPersonName = findViewById(R.id.editTextTextPersonName);
+        editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
+        editTextTextConfPassword = findViewById(R.id.editTextTextConfPassword);
+        btnTextViewLogIn = findViewById(R.id.textViewLogInbtn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SignUpActivity.this,"Signed up",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(SignUpActivity.this,MainActivity.class));
+            }
+        });
+
+        btnTextViewLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this,LogInActivity.class));
+            }
+        });
+
+
+
+
+
+
+
+
     }
 }
