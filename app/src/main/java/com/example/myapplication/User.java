ae82ee6eeec70198a454df65834e994ed87fd700
Eren <erenuslu.bnd@gmail.com>
2023-05-21 04:06:41 +0300

    //created class

---
+++ app/src/main/java/com/example/myapplication/User.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import android.graphics.*;
+
+
+public class User {
+
+    public int id;   //unique user code, it contains 6 digit [0-9]
+    public String userName;         // username
+    public String password;         //password
+    public String mail;             //mail
+
+
+}
