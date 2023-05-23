53deb40b8c7294b5c0c4b671609973225028a5bf
Mert Özkaya <mert.ozkaya@ug.bilkent.edu.tr>
2023-05-23 16:39:58 +0300

    Added forget password xml files

--- app/src/main/java/com/example/myapplication/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/myapplication/ForgotPasswordActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
@@ -10,5 +14,8 @@ public class ForgotPasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        Button forgetPassButton = findViewById(R.id.btnSendCode);
+        EditText email = findViewById(R.id.EmailAddressForgetPass);
+        
     }
 }
