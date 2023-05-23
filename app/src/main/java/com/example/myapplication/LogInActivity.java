ac668dd3e4c7bc2fbddc993307530983e0e7b993
Emir Eroğlu <emireroglu2k@gmail.com>
2023-05-23 23:03:25 +0300

    completed Login Activity

--- app/src/main/java/com/example/myapplication/LogInActivity.java
+++ app/src/main/java/com/example/myapplication/LogInActivity.java
@@ -1,15 +1,21 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LogInActivity extends AppCompatActivity {
@@ -27,8 +33,8 @@ public class LogInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-        editTextEmailLoginPage = findViewById(R.id.editTextTextEmailAddress);
-        editTextPasswordLoginPage = findViewById(R.id.editTextTextEmailAddress_loginpage);
+        editTextEmailLoginPage = findViewById(R.id.editTextTextEmailAddress_loginpage);
+        editTextPasswordLoginPage = findViewById(R.id.editTextTextPassword);
         textViewForgotPassword = findViewById(R.id.textViewForgotPassword);
         textViewSignUp = findViewById(R.id.textViewSignUp);
         btnLogin = findViewById(R.id.btnLogIn);
@@ -38,7 +44,6 @@ public class LogInActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //startActivity(new Intent(LogInActivity.this,MainActivity.class));
                 loginUser();
             }
         });
@@ -59,7 +64,31 @@ public class LogInActivity extends AppCompatActivity {
 
     }
 
-    private void loginUser() {
+    private void loginUser(){
+        String loginEmail = editTextEmailLoginPage.getText().toString();
+        String loginPassword = editTextPasswordLoginPage.getText().toString();
 
+        if(TextUtils.isEmpty(loginEmail)) {
+            editTextEmailLoginPage.setError("Email cannot be empty.");
+            editTextEmailLoginPage.requestFocus();
+        }
+        else if(TextUtils.isEmpty(loginPassword)) {
+            editTextPasswordLoginPage.setError("Password cannot be empty.");
+            editTextPasswordLoginPage.requestFocus();
+        }
+        else {
+            mAuth.signInWithEmailAndPassword(loginEmail,loginPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                        Toast.makeText(LogInActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LogInActivity.this,MainActivity.class));
+                    }
+                    else {
+                        Toast.makeText(LogInActivity.this,"Login Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
