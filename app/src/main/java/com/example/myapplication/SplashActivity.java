2414b35e9e189f2eb41d8f3155071865bc41352d
Mert Özkaya <mert.ozkaya@ug.bilkent.edu.tr>
2023-05-27 21:23:28 +0300

    Helped emir

--- app/src/main/java/com/example/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -6,15 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashActivity extends AppCompatActivity {
+    FirebaseUser fUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = (new Intent(SplashActivity.this,LogInActivity.class));
+                fUser = FirebaseAuth.getInstance().getCurrentUser();
+                Intent intent;
+                if(fUser == null)
+                {
+                     intent= (new Intent(SplashActivity.this,LogInActivity.class));
+                }
+                else
+                {
+                    intent = new Intent(SplashActivity.this,MainActivity.class);
+                }
                 startActivity(intent);
                 finish();
             }
